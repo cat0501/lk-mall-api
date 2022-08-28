a130e37a92bcdcc2a0fb44c0903f7f30bc7e56cf
Lemonade19 <pxzjl123@icloud.com>
2022-08-28 15:57:08 +0800

    # 后端初始化环境搭建

---
+++ src/main/java/com/mall/Bootstrap.java
@@ -0,0 +1,16 @@
+package com.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangjianlin
+ * @description 启动类
+ * @date 2022/8/28 3:18 PM
+ */
+@SpringBootApplication
+public class Bootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap.class, args);
+    }
+}
---
+++ src/main/java/com/mall/common/Constants.java
@@ -0,0 +1,9 @@
+package com.mall.common;
+
+/**
+ * @author zhangjianlin
+ * @description
+ * @date 2022/8/28 3:37 PM
+ */
+public class Constants {
+}
