83f36c7ad4d6fc66e2c07d0cf3ec6a13194b9b9b
Lemonade19 <pxzjl123@icloud.com>
2022-08-30 21:14:52 +0800

    # 新增swagger2配置类

---
+++ src/main/java/com/lk/mall/config/Swagger2Config.java
@@ -0,0 +1,63 @@
+package com.lk.mall.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhangjianlin
+ * @description
+ * @date 2022/8/30 9:13 PM
+ */
+//@Configuration
+//@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.newbee.mall.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(setHeaderToken());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("商城接口文档")
+                .description("swagger接口文档")
+                .termsOfServiceUrl("")
+                .version("1.0")
+                .build();
+    }
+
+    /**
+     * @description 设置swagger文档中全局参数
+     */
+    private List<Parameter> setHeaderToken() {
+        List<Parameter> pars = new ArrayList<>();
+        ParameterBuilder userId = new ParameterBuilder();
+        userId.name("token")
+                .description("用户TOKEN")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(true).build();
+        pars.add(userId.build());
+        return pars;
+    }
+}
+
