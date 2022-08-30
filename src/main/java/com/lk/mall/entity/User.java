cb6337b708d175779ae454126f5527c5313cf065
Lemonade19 <pxzjl123@icloud.com>
2022-08-30 20:50:17 +0800

    # update

---
+++ src/main/java/com/lk/mall/entity/User.java
@@ -0,0 +1,70 @@
+package com.lk.mall.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.time.LocalDateTime;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author lak
+ * @since 2022-08-30
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("mall_user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 用户主键id
+     */
+    @TableId(value = "user_id", type = IdType.AUTO)
+    private Long userId;
+
+    /**
+     * 用户昵称
+     */
+    private String nickName;
+
+    /**
+     * 登陆名称(默认为手机号)
+     */
+    private String loginName;
+
+    /**
+     * MD5加密后的密码
+     */
+    private String passwordMd5;
+
+    /**
+     * 个性签名
+     */
+    private String introduceSign;
+
+    /**
+     * 注销标识字段(0-正常 1-已注销)
+     */
+    private Integer isDeleted;
+
+    /**
+     * 锁定标识字段(0-未锁定 1-已锁定)
+     */
+    private Integer lockedFlag;
+
+    /**
+     * 注册时间
+     */
+    private LocalDateTime createTime;
+
+
+}
---
+++ src/main/java/com/lk/mall/service/impl/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.lk.mall.service.impl;
+
+import com.lk.mall.entity.User;
+import com.lk.mall.dao.UserMapper;
+import com.lk.mall.service.IUserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author lak
+ * @since 2022-08-30
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+}
