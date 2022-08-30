ff551b074e400d3c210623ba7db4b2c6d097732f
doubledogss <lucklak@163.com>
2022-08-30 20:25:40 +0800

    lak

---
+++ src/main/java/com/lk/mall/dao/UserMapper.java
@@ -0,0 +1,16 @@
+package com.lk.mall.dao;
+
+import com.lk.mall.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author lak
+ * @since 2022-08-30
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/main/java/com/lk/mall/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.lk.mall.service;
+
+import com.lk.mall.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author lak
+ * @since 2022-08-30
+ */
+public interface IUserService extends IService<User> {
+
+}
